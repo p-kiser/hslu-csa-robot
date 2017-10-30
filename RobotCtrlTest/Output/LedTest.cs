2ba1edd12066335e1a98c9d770dcf3a7785d3f12
Lucien Zürcher <l.zuercher@tincan.ch>
2017-10-30 23:32:59 +0100

    chore: added LedTest

---
+++ RobotCtrlTest/Output/LedTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RobotCtrlTest.Output
+{
+    [TestClass]
+    public class LedTest
+    {
+        [TestMethod]
+        public void TestLedStateChangedInvoked()
+        {
+            // TODO
+        }
+    }
+}
