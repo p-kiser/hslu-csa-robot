b28ddaa81d4c27fcf329afde1a5d941673f9a80e
Lucien Zürcher <l.zuercher@tincan.ch>
2017-11-18 10:30:54 +0100

    feat: setup form

--- TestTracks/Form1.Designer.cs
+++ TestTracks/Form1.Designer.cs
@@ -30,33 +30,50 @@
         private void InitializeComponent()
         {
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
-            this.button1 = new System.Windows.Forms.Button();
             this.runTurn = new RobotView.RunTurn();
+            this.runArc = new RobotView.RunArc();
+            this.runLine = new RobotView.RunLine();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(72, 20);
-            this.button1.TabIndex = 0;
             // 
             // runTurn
             // 
-            this.runTurn.Acceleration = 0F;
+            this.runTurn.Acceleration = 0.5F;
             this.runTurn.Drive = null;
-            this.runTurn.Location = new System.Drawing.Point(28, 18);
+            this.runTurn.Location = new System.Drawing.Point(48, 313);
             this.runTurn.Name = "runTurn";
-            this.runTurn.Size = new System.Drawing.Size(454, 69);
-            this.runTurn.Speed = 0F;
+            this.runTurn.Size = new System.Drawing.Size(454, 98);
+            this.runTurn.Speed = 1F;
             this.runTurn.TabIndex = 0;
+            // 
+            // runArc
+            // 
+            this.runArc.Acceleration = 0.5F;
+            this.runArc.Drive = null;
+            this.runArc.Location = new System.Drawing.Point(548, 159);
+            this.runArc.Name = "runArc";
+            this.runArc.Size = new System.Drawing.Size(454, 115);
+            this.runArc.Speed = 1F;
+            this.runArc.TabIndex = 0;
+            // 
+            // runLine
+            // 
+            this.runLine.Acceleration = 0.5F;
+            this.runLine.Drive = null;
+            this.runLine.Length = 1F;
+            this.runLine.Location = new System.Drawing.Point(531, 38);
+            this.runLine.Name = "runLine";
+            this.runLine.Size = new System.Drawing.Size(454, 115);
+            this.runLine.Speed = 1F;
+            this.runLine.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(638, 455);
+            this.ClientSize = new System.Drawing.Size(1058, 610);
             this.Controls.Add(this.runTurn);
+            this.Controls.Add(this.runArc);
+            this.Controls.Add(this.runLine);
             this.Menu = this.mainMenu1;
             this.Name = "Form1";
             this.Text = "Form1";
@@ -66,8 +83,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private RobotView.RunTurn runTurn;
+        private RobotView.RunArc runArc;
+        private RobotView.RunLine runLine;
     }
 }
 
