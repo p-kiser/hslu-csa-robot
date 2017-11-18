ee823aeca75c48f5b64f5976868471cf4f193925
pascal <pascal.kiser@stud.hslu.ch>
2017-11-18 10:12:11 +0100

    feat: added consoleView to Testat form

--- Testat/Form1.Designer.cs
+++ Testat/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Testat
+﻿using RobotView;
+
+namespace Testat
 {
     partial class Form1
     {
@@ -30,14 +32,46 @@
         private void InitializeComponent()
         {
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
-            this.Menu = this.mainMenu1;
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.consoleView = new RobotView.ConsoleView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            // 
+            // consoleView
+            // 
+            this.consoleView.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.consoleView.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.consoleView.Location = new System.Drawing.Point(16, 343);
+            this.consoleView.Name = "consoleView";
+            this.consoleView.Size = new System.Drawing.Size(176, 91);
+            this.consoleView.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.Text = "Form1";
             this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(638, 455);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.consoleView);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private ConsoleView consoleView;
     }
 }
 
